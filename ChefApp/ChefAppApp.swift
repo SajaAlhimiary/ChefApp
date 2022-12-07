6e0f0470cbf42a67227e543f5e74062b6748430c
Saja Alhimiary <sajaalhimiary@yahoo.com>
2022-12-07 12:34:39 +0300

    Initial Commit

---
+++ ChefApp/ChefAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefAppApp.swift
+//  ChefApp
+//
+//  Created by Saja Alhimiary on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChefAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
