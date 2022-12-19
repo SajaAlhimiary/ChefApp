3f4b8bccbd468ec6cbd2b9840c0ff13515219d97
Saja Alhimiary <sajaalhimiary@yahoo.com>
2022-12-19 10:22:15 +0300

    database

--- ChefApp/ChefList.swift
+++ ChefApp/ChefList.swift
@@ -9,63 +9,83 @@ import SwiftUI
 import CloudKit
 
 struct ChefList: View {
-    @State var chefList : [Chef] = []
+    @State var chefList: [Chef] = []
+    @State var cuisines: [String] = []
+    
     var body: some View {
         NavigationView{
-                    List{
-                        ForEach(chefList) { chef  in
-                            HStack(spacing: 2){
-                                Image("avatar\(Int.random(in: 1..<7))")
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 64, height: 64)
-                                    .clipShape(Circle())
-                                    .padding(.vertical)
-                                    .padding(.horizontal, 2)
-                                
-                                VStack(alignment: .leading, spacing:6){
-                                    Text("Chef. \(chef.firstName) \(chef.lastName)")
-                                        .font(.title3)
-                                        .fontWeight(.semibold)
-
-//                                    Text("Bio: \(chef.bio)")
-                                    Text("Rating: \(chef.rating)")
-
+            List{
+                ForEach(chefList) { chef  in
+                    HStack(spacing: 2){
+                        Image("avatar\(Int.random(in: 1..<7))")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 64, height: 64)
+                            .clipShape(Circle())
+                            .padding(.vertical)
+                            .padding(.horizontal, 2)
+                        
+                        VStack(alignment: .leading, spacing:6){
+                            Text("Chef. \(chef.firstName) \(chef.lastName)")
+                                .font(.title3)
+                                .fontWeight(.semibold)
+                            
+                            //                                    Text("Bio: \(chef.bio)")
+                            Text("Rating: \(String(format: "%.1f", (chef.rating)))")
+                            
+                            VStack {
+                                ForEach(cuisines, id: \.self) { item in
+                                    Text(item)
                                 }
-                                .padding(6)
                             }
-                        }
+//                            VStack {
+//                                ForEach(cuisines) { item in
+//                                    Text(item.capitalized)
+//                                        .padding()
+//                                }
+//                            }
+//                            Text("Ready or not, here I come!")
+//                        }
                     }
-                    .listStyle(.plain)
-                    .onAppear{
-                        fetchChef()
-                    }
-                    .navigationTitle("Chef")
+                    .padding(6)
                 }
             }
-            
-            //View Model
-            func fetchChef(){
-                
-                let container = CKContainer(identifier: "iCloud.Chef")
-                let predicate = NSPredicate(value: true)
-                let query = CKQuery(recordType: "Chef", predicate: predicate)
-                
-                let operations = CKQueryOperation(query: query)
-                operations.recordMatchedBlock = {  recordId, result in
-                    
-                    switch result {
-                    case .success( let records):
-                        let chef = Chef(record: records)
-                        self.chefList.append(chef)
-                    case .failure( let error):
-                        print(error.localizedDescription)
-                    }
-                    
-                }
-                
-                container.publicCloudDatabase.add(operations)
+        }
+        .listStyle(.plain)
+        .onAppear{
+            fetchChef()
+        }
+        .navigationTitle("Chef")
     }
+}
+
+//View Model
+func fetchChef(){
+    
+    let container = CKContainer(identifier: "iCloud.Chef")
+    let predicate = NSPredicate(value: true)
+    let query = CKQuery(recordType: "Chef", predicate: predicate)
+    
+    let operations = CKQueryOperation(query: query)
+    operations.recordMatchedBlock = {  recordId, result in
+        
+        switch result {
+        case .success( let records):
+            print(records)
+            let chef = Chef(record: records)
+            let cousine = records.value(forKey: "cuisine")
+            self.cuisines = cousine as! [String]
+            print(self.cuisines)
+            
+            self.chefList.append(chef)
+        case .failure( let error):
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+    container.publicCloudDatabase.add(operations)
+}
 }
 
 struct ChefList_Previews: PreviewProvider {
@@ -80,20 +100,20 @@ struct Chef: Identifiable {
     let id: CKRecord.ID
     let firstName : String
     let lastName : String
-//    let reviews : [String]
+    //    let reviews : [String]
     let rating : Double
-//    let cuisine : [String]
-//    let photos : [String]
-//    let bio : String
-//    let messages : [String]
-   
+    let cuisine : [String]
+    //    let photos : [String]
+    //    let bio : String
+    //    let messages : [String]
+    
     init(record:CKRecord) {
         self.id = record.recordID
         self.firstName = record["firstName"]  as? String ?? "N/A"
         self.lastName = record["lastName"] as? String ?? "N/A"
         self.rating = record["rating"] as? Double ?? 0
-//        self.bio = record["bio"] as? String ?? "N/A"
-//        self.cuisine = record["cuisine"] as? [String]?? "N/A"
+        //        self.bio = record["bio"] as? String ?? "N/A"
+        self.cuisine = record["cuisine"] as? [String] ?? []
         
     }
     
